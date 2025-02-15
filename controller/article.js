d211073d30c20cbd5d801ca8ac620fe9d05738b9
Rielly Kirk <Riellykirk13@gmail.com>
2025-02-15 13:58:14 -0500

    finshed model and added coment reqiurments for controller

--- controller/article.js
+++ controller/article.js
@@ -1,4 +1,22 @@
 import express from 'express';
 
+//model ref
+import Article from '../models/articles.js';
+
+// router to handle requests
+const router = express.Router();
+
+//get all
+
+//get by title
+
+//get by author
+
+//post new article
+
+//update article
+
+//delete article
+
 // make controller public
 export default express.Router();
--- models/articles.js
+++ models/articles.js
@@ -1,5 +1,27 @@
 //import mongoose from "mongoose";
+import mongoose from "mongoose";
 
 // Schema
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  date: { //default date saves 2024-03-16T14:30:00.000Z 
+    type: Date,
+    default: () => new Date().toISOString().split('T')[0] 
+  }  
+});
 
 //inherit CRUD methods from mongoose
+const Article = mongoose.model('Article', articleSchema);
+
+export default Article;
