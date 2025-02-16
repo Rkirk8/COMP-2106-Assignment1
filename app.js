0bffa32be4e1c71f29a4c6828c0c472ebf353993
Rielly Kirk <Riellykirk13@gmail.com>
2025-02-15 21:38:14 -0500

    added swagger and I belive it should all work fine just need to debug

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import path from 'path';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
 
 // Controller
-import articleController from './controller/article.js';
+import articleController from './controller/articles.js';
 
 // Config
 const app = express();
@@ -15,8 +19,27 @@ mongoose.connect(process.env.DB, {})
 .then((res) => console.log('Connected to MongoDB'))
   .catch((err) => console.log(`Connection Failure: ${err}`));
 
+// swagger api config
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Assignment 1 API",
+            version: "1.0.0",
+            description: "Assignment 1 API",
+        },
+    },
+    apis: ['./controller/*.js'],
+};
+
+const __dirname = path.resolve();
+const __swaggerDistPath = path.join(__dirname, "node_modules", "swagger-ui-dist");
+const openapiSpecs = swaggerJSDoc(options);
+app.use('/api-docs', express.static(__swaggerDistPath, { index: false }), swaggerUi.serve, swaggerUi.setup(openapiSpecs));
+
+
 // Map url to controller
-app.use('/article', articleController);
+app.use('/v1/articles', articleController);
 
 // Start server
 app.listen(3000, () => { console.log("Server started on port 3000")});
---
+++ controller/articles.js
@@ -0,0 +1,189 @@
+import express from 'express';
+
+// Model reference
+import Article from '../models/articles.js';
+
+// Create Express router object to handle HTTP request/response calls
+const router = express.Router();
+
+/**
+ * @swagger
+ * /api/v1/articles:
+ *   get:
+ *     summary: Find all articles
+ *     tags:
+ *       - Article
+ *     responses:
+ *       200:
+ *         description: Returns all articles
+ */
+router.get('/', async (req, res) => {
+    let articles = await Article.find();
+    return res.status(200).json(articles);
+});
+
+/**
+ * @swagger
+ * /api/v1/articles/{id}:
+ *   get:
+ *     summary: Find selected article by id
+ *     tags:
+ *       - Article
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         schema:
+ *           type: string
+ *           required: true
+ *     responses:
+ *       200:
+ *         description: Returns single article
+ *       404:
+ *         description: Not found
+ */
+router.get('/:id', async (req, res) => {
+    let article = await Article.findById(req.params.id);
+    if (!article) return res.status(404).json({ err: "Not Found" });
+    return res.status(200).json(article);
+});
+
+/**
+ * @swagger
+ * /api/v1/articles/author/{author}:
+ *   get:
+ *     summary: Find all articles by author
+ *     tags:
+ *       - Article
+ *     parameters:
+ *       - name: author
+ *         in: path
+ *         schema:
+ *           type: string
+ *           required: true
+ *     responses:
+ *       200:
+ *         description: Returns all articles by author
+ */
+router.get('/author/:author', async (req, res) => {
+    let articles = await Article.find({ author: req.params.author });
+    return res.status(200).json(articles);
+});
+
+/**
+ * @swagger
+ * /api/v1/articles:
+ *   post:
+ *     summary: Create new article from request body
+ *     tags:
+ *       - Article
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 required: true
+ *               author:
+ *                 type: string
+ *                 required: true
+ *               content:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Resource created
+ *       400:
+ *         description: Bad Request
+ */
+router.post('/', async (req, res) => {
+    try {
+        await Article.create(req.body);
+        return res.status(201).json();
+    } catch (err) {
+        return res.status(400).json({ err: `Bad Request: ${err.message}` });
+    }
+});
+
+/**
+ * @swagger
+ * /api/v1/articles/{id}:
+ *   put:
+ *     summary: Update selected article from request body
+ *     tags:
+ *       - Article
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               _id:
+ *                 type: string
+ *                 required: true
+ *               title:
+ *                 type: string
+ *                 required: true
+ *               author:
+ *                 type: string
+ *                 required: true
+ *               content:
+ *                 type: string
+ *     responses:
+ *       204:
+ *         description: No content
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: Not found
+ */
+router.put('/:id', async (req, res) => {
+    try {
+        let article = await Article.findById(req.params.id);
+        if (!article) return res.status(404).json({ err: "Not Found" });
+        if (req.params.id != req.body._id) {
+            return res.status(400).json({ err: "Bad Request: IDs do not match" });
+        }
+        await Article.findByIdAndUpdate(req.params.id, req.body);
+        return res.status(204).json();
+    } catch (err) {
+        return res.status(400).json({ err: `Bad Request: ${err.message}` });
+    }
+});
+
+/**
+ * @swagger
+ * /api/v1/articles/{id}:
+ *   delete:
+ *     summary: Find and delete selected article by id
+ *     tags:
+ *       - Article
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         schema:
+ *           type: string
+ *           required: true
+ *     responses:
+ *       204:
+ *         description: No content
+ *       404:
+ *         description: Not found
+ */
+router.delete('/:id', async (req, res) => {
+    let article = await Article.findById(req.params.id);
+    if (!article) return res.status(404).json({ err: "Not Found" });
+    await Article.findByIdAndDelete(req.params.id);
+    return res.status(204).json();
+});
+
+// Make controller public
+export default router;
